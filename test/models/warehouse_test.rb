7ad8e5fdf2689eca0993fb668093ac9cddae72b7
Jeff Cohen <cohen.jeff@gmail.com>
2019-05-14 11:54:48 -0500

    Small update

--- test/models/warehouse_test.rb
+++ test/models/warehouse_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class WarehouseTest < ActiveSupport::TestCase
 
   def test_one_shipment_per_item
-    cart = ShoppingCart.new
+    cart = ShoppingCart.new # something
     cart.add('B01DFKC2SO')
     cart.add('B01JP436TS')
 
